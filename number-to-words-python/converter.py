ee3e817c42957d2333f91ca8e8d46f43664cf141
Sulu Edward Julianto <93524287+SuluJulianto@users.noreply.github.com>
2023-12-17 05:38:23 +0700

    Update Config and Code

--- number-to-words-python/converter.py
+++ number-to-words-python/converter.py
@@ -1,32 +1,32 @@
 wrd = {
-         0:"",
-         1:"one ",
-         2:"two ",
-         3:"three ",
-         4:"four ",
-         5:"five ",
-         6:"six ",
-         7:"seven ",
-         8:"eight ",
-         9:"nine ",
-         10:"ten ",
-         11:"eleven ",
-         12:"twelve ",
-         13:"thirteen ",
-         14:"fourteen ",
-         15:"fifteen ",
-         16:"sixteen ",
-         17:"seventeen ",
-         18:"eighteen ",
-         19:"nineteen ",
-         20:"twenty" ,
-         30:"thirty",
-         40:"forty", 
-         50:"fifty",
-         60:"sixty",
-         70:"seventy",
-         80:"eighty",
-         90:"ninty"
+    0: "",
+    1: "one ",
+    2: "two ",
+    3: "three ",
+    4: "four ",
+    5: "five ",
+    6: "six ",
+    7: "seven ",
+    8: "eight ",
+    9: "nine ",
+    10: "ten ",
+    11: "eleven ",
+    12: "twelve ",
+    13: "thirteen ",
+    14: "fourteen ",
+    15: "fifteen ",
+    16: "sixteen ",
+    17: "seventeen ",
+    18: "eighteen ",
+    19: "nineteen ",
+    20: "twenty ",
+    30: "thirty ",
+    40: "forty ",
+    50: "fifty ",
+    60: "sixty ",
+    70: "seventy ",
+    80: "eighty ",
+    90: "ninety "
 }
 
 place = {
@@ -52,50 +52,54 @@ def prt(num, length, last_index):
     t = x % 10
     x //= 10
     h = x % 10
-    
+
     if num < 20:
         st = wrd[num]
-        if h == 0 and t == 0 and o == 0:
-            value = False
+        value = False if h == 0 and t == 0 and o == 0 else True
     else:
         if h > 0:
             st += wrd[h] + place[1]
-        
-        if ((t * 10 + o) < 20):
+        if t * 10 + o < 20:
             st += wrd[t * 10 + o]
         else:
             if t > 0:
                 st += wrd[t * 10]
                 if o > 0:
-                    st += "-"
+                    st += " "
             if o > 0:
                 st += wrd[o]
-    
+
     if last_index > 1 and value:
         st += place[length]
-        
     return st
 
-x = input()
-myst = ""
-num = int(x)
+def convert_to_words(input):
+    myst = ""
+    num = int(input)
 
-if num < 0:
-    myst = "Negative "
-    x = str(abs(num))
+    if num < 0:
+        myst = "Negative "
+        input = str(abs(num))
 
-if num == 0:
-    myst = "zero"
+    if num == 0:
+        myst = "zero"
 
-if len(x) > 21:
-    print('Please enter a number smaller than or equal to 21 digits')
-else:
-    print("Your number is:", int(x).__format__(','))
-    temp_list = main(x)
-    temp_len = len(temp_list)
-    
-    for i in temp_list:
-        myst += prt(int(i), temp_len, temp_len)
-        temp_len -= 1
+    if len(input) > 21:
+        return 'Please enter a number smaller than or equal to 21 digits'
+    else:
+        number_output = f"Your number is: {int(input):,}"
+        print(number_output)
+        
+        temp_list = main(input)
+        temp_len = len(temp_list)
+        
+        for i in temp_list:
+            myst += prt(int(i), temp_len, temp_len)
+            temp_len -= 1
+        
+        return myst.strip()
 
-print(myst)
+# Example usage
+user_input = input("Enter a number: ")
+words_output = convert_to_words(user_input)
+print("In words:", words_output)
